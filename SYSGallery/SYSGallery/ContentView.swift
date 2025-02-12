c9b928c194eef2b124a357558e98b564acaaf1bc
Oleksandr Sysenko <alexandr.sysenko@gmail.com>
2025-02-12 19:11:49 +0200

    init project

---
+++ SYSGallery/SYSGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SYSGallery
+//
+//  Created by Oleksandr Sysenko on 12.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SYSGallery/SYSGallery/SYSGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SYSGalleryApp.swift
+//  SYSGallery
+//
+//  Created by Oleksandr Sysenko on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct SYSGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
